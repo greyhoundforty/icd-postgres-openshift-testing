e35a85abd8c45153b34fb757e4c384d1c0c9b668
Ryan Tiffany <ryantiffany@fastmail.com>
2021-10-21 11:31:40 -0500

    Test without the ibmcos intermediary

--- get-info/server.js
+++ get-info/server.js
@@ -71,18 +71,15 @@ var config = {
     signatureVersion: 'iam',
 };
 
-let itemName = nodejsbinding;
-
 var bucketName = process.env.bucket;
-
 
 var cos = new IBM.S3(config);
 
-function createTextFile(bucketName, itemName, fileText) {
-    console.log(`Creating new item: ${itemName}`);
+function createTextFile(bucketName, fileText) {
+    console.log(`Creating new item: nodejsbinding`);
     return cos.putObject({
         Bucket: bucketName, 
-        Key: itemName, 
+        Key: nodejsbinding, 
         Body: process.env.postgresbinding
     }).promise()
     .then(() => {
@@ -92,96 +89,3 @@ function createTextFile(bucketName, itemName, fileText) {
         console.error(`ERROR: ${e.code} - ${e.message}\n`);
     });
 }
-
-// // We now take the first bound PostgreSQL service and extract its credentials object from BINDING
-// let postgresconn = credentials.connection.postgres;
-// let caCert = new Buffer.from(postgresconn.certificate.certificate_base64, 'base64').toString();
-// let connectionString = postgresconn.composed[0];
-
-// // set up a new client using our config details
-// let client = new pg.Client({ connectionString: connectionString,
-//     ssl: {
-//         ca: caCert
-//     }
-//  });
-
-// client.connect(function(err) {
-//     if (err) {
-//         console.log(err);
-//         process.exit(1);
-//     } else {
-//         client.query(
-//             "CREATE TABLE IF NOT EXISTS words (word varchar(256) NOT NULL, definition varchar(256) NOT NULL)",
-//             function(err, result) {
-//                 if (err) {
-//                     console.log(err);
-//                 }
-//             }
-//         );
-//     }
-// });
-
-// // Add a word to the database
-// function addWord(word, definition) {
-//     return new Promise(function(resolve, reject) {
-//         let queryText = "INSERT INTO words(word,definition) VALUES($1, $2)";
-//         client.query(
-//             queryText, [word, definition],
-//             function(error, result) {
-//                 if (error) {
-//                     reject(error);
-//                 } else {
-//                     resolve(result);
-//                 }
-//             }
-//         );
-//     });
-// }
-
-// // Get words from the database
-// function getWords() {
-//     return new Promise(function(resolve, reject) {
-//         client.query("SELECT * FROM words ORDER BY word ASC", function(
-//             err,
-//             result
-//         ) {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(result.rows);
-//             }
-//         });
-//     });
-// }
-
-// // We can now set up our web server. First up we set it to serve static pages
-// app.use(express.static(__dirname + "/public"));
-
-// // The user has clicked submit to add a word and definition to the database
-// // Send the data to the addWord function and send a response if successful
-// app.put("/words", function(request, response) {
-//     addWord(request.body.word, request.body.definition)
-//         .then(function(resp) {
-//             response.send(resp);
-//         })
-//         .catch(function(err) {
-//             console.log(err);
-//             response.status(500).send(err);
-//         });
-// });
-
-// // Read from the database when the page is loaded or after a word is successfully added
-// // Use the getWords function to get a list of words and definitions from the database
-// app.get("/words", function(request, response) {
-//     getWords()
-//         .then(function(words) {
-//             response.send(words);
-//         })
-//         .catch(function(err) {
-//             console.log(err);
-//             response.status(500).send(err);
-//         });
-// });
-
-// // Listen for a connection.
-
